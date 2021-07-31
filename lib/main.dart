bcfd2dc8017f4ab5881494ae13bbcc503d99aa9c
K1A2 <jckim0307@gmail.com>
2021-07-31 18:22:08 +0900

    check life cycle

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ void main() {
 class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
+    print('createState');
     return _MyApp();
   }
 }
@@ -18,6 +19,7 @@ class _MyApp extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
+    print('build');
     return MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
@@ -45,4 +47,18 @@ class _MyApp extends State<MyApp> {
         )
     );
   }
+
+  @override
+  void initState() {
+    super.initState();
+    print('initState');
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    print('didChangeDependencies');
+  }
+
+
 }
