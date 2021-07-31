d0002f7970fe921ff8a068e7a859d1ff74eb3e32
K1A2 <jckim0307@gmail.com>
2021-07-31 21:33:24 +0900

    make simple calculator

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,109 @@
 import 'package:flutter/material.dart';
-import 'imageWidget.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  static const String _title = 'Widget Exemple';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material Flutter App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue
+      title: _title,
+      home: WidgetApp(),
+    );
+  }
+}
+
+class WidgetApp extends StatefulWidget {
+  WidgetApp({Key key}) : super(key: key);
+  @override
+  _WidgetExempleState createState() => _WidgetExempleState();
+}
+
+class _WidgetExempleState extends State<WidgetApp> {
+  String sum = '';
+  TextEditingController value1 = TextEditingController();
+  TextEditingController value2 = TextEditingController();
+
+  List _buttonList = ['더하기', '빼기', '곱하기', '나누기'];
+  List<DropdownMenuItem<String>> _dropDownMenuItems = new List();
+  String _buttonText;
+
+  @override
+  void initState() {
+    super.initState();
+    for (var item in _buttonList) {
+      _dropDownMenuItems.add(DropdownMenuItem(child: Text(item), value: item,));
+    }
+    _buttonText = _dropDownMenuItems[0].value;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Widget Exemple'),
       ),
-      home: ImageWidgetApp(),
+      body: Container(
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.all(25),
+                child: Text(
+                  '결과 : $sum',
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 20, right: 20),
+                child: TextField(keyboardType: TextInputType.number, controller: value1,),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 20, right: 20),
+                child: TextField(keyboardType: TextInputType.number, controller: value2,),
+              ),
+              Padding(
+                padding: EdgeInsets.all(15),
+                child: ElevatedButton(
+                  child: Row(
+                    children: <Widget>[
+                      Icon(Icons.add),
+                      Text(_buttonText)
+                    ],
+                  ),
+                    onPressed: () {
+                      setState(() {
+                        var result;
+                        if (_buttonText == '더하기') {
+                          result = int.parse(value1.value.text) + int.parse(value2.value.text);
+                        } else if (_buttonText == '빼기') {
+                          result = int.parse(value1.value.text) - int.parse(value2.value.text);
+                        } else if (_buttonText == '곱하기') {
+                          result = int.parse(value1.value.text) * int.parse(value2.value.text);
+                        } else if (_buttonText == '나누기') {
+                          result = int.parse(value1.value.text) / int.parse(value2.value.text);
+                        }
+                        sum = '$result';
+                      });
+                    }
+                    )
+              ),
+              Padding(
+                padding: EdgeInsets.all(15),
+                child: DropdownButton(
+                  items: _dropDownMenuItems,
+                  onChanged: (value) {
+                    setState(() {
+                      _buttonText = value;
+                    });
+                  }, value: _buttonText,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
