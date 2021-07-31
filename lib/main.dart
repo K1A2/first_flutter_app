4e950d07e4a9a759e0690fcb9b542191c6a5fa8c
K1A2 <jckim0307@gmail.com>
2021-07-31 18:29:26 +0900

    use material design

--- lib/main.dart
+++ lib/main.dart
@@ -1,64 +1,38 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    print('createState');
-    return _MyApp();
-  }
-}
-
-class _MyApp extends State<MyApp> {
-  var switchValue = false;
-  String test = 'hello';
-  Color _color = Colors.blue;
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    print('build');
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-        ),
-        darkTheme: ThemeData.light(),
-        home: Scaffold(
-          body: Center(
-            child: ElevatedButton(
-              child: Text('$test'),
-              onPressed: () {
-                if (test == 'hello') {
-                  setState(() {
-                    test = 'flutter';
-                  });
-                } else {
-                  setState(() {
-                    test = 'hello';
-                  });
-                }
-              },
-            )
-          ),
-        )
+      title: 'Material Flutter App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue
+      ),
+      home: MaterialFlutterApp(),
     );
   }
+}
 
+class MaterialFlutterApp extends StatefulWidget {
   @override
-  void initState() {
-    super.initState();
-    print('initState');
+  State<StatefulWidget> createState() {
+    return _MaterialFlutterApp();
   }
+}
 
+class _MaterialFlutterApp extends State<MaterialFlutterApp> {
   @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    print('didChangeDependencies');
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Material Design App'),),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+
+        },
+      ),
+    );
   }
-
-
 }
